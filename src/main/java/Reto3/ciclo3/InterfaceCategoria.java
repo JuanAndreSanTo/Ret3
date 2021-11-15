bf6f2b9463e4c2f6ceea35b03dfcfd4f735ebb0f
JuanAndreSanTo <juan.sanchez3.mt@correo.usa.edu.co>
2021-11-15 00:42:59 -0500

    first commit

---
+++ src/main/java/Reto3/ciclo3/InterfaceCategoria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reto3.ciclo3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @JUAN SANCHEZ
+ */
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer> {
+    
+}
---
+++ src/main/java/Reto3/ciclo3/InterfaceCliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3.ciclo3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @JUAN SANCHEZ
+ */
+public interface InterfaceCliente extends CrudRepository<Cliente,Integer> {
+    
+}
